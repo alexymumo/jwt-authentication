c3d3538b27d93b58195a48712ac3b56d56aef201
Alex Mumo <alexymumo2000@gmail.com>
2025-03-18 14:33:16 +0300

    Add post end point

---
+++ src/main/java/com/technobraintask/ecommerce_api/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.technobraintask.ecommerce_api.controller;
+
+import com.technobraintask.ecommerce_api.models.Product;
+import com.technobraintask.ecommerce_api.repository.ProductRepository;
+import com.technobraintask.ecommerce_api.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Product> registerProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.createProduct(product));
+    }
+}
--- src/main/java/com/technobraintask/ecommerce_api/controller/UserController.java
+++ src/main/java/com/technobraintask/ecommerce_api/controller/UserController.java
@@ -4,12 +4,10 @@ import com.technobraintask.ecommerce_api.models.User;
 import com.technobraintask.ecommerce_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/users")
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     @Autowired
