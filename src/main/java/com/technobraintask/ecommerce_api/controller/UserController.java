19de5a1fa470e83d7ee026a280f9b53bfa4a5300
Alex Mumo <alexymumo2000@gmail.com>
2025-03-21 11:16:34 +0300

    Add documentation tags

--- src/main/java/com/technobraintask/ecommerce_api/controller/UserController.java
+++ src/main/java/com/technobraintask/ecommerce_api/controller/UserController.java
@@ -2,10 +2,16 @@ package com.technobraintask.ecommerce_api.controller;
 
 import com.technobraintask.ecommerce_api.dto.LoginDto;
 import com.technobraintask.ecommerce_api.dto.RegisterDto;
+import com.technobraintask.ecommerce_api.models.Product;
 import com.technobraintask.ecommerce_api.models.User;
 import com.technobraintask.ecommerce_api.response.LoginResponse;
 import com.technobraintask.ecommerce_api.security.service.JwtService;
 import com.technobraintask.ecommerce_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,13 +28,28 @@ public class UserController {
     private JwtService jwtService;
 
 
-
+    @Operation(summary = "register new user")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "create new user", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+                    @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+                    @ApiResponse(responseCode = "403",description = "bad credentials",content = @Content),
+                    @ApiResponse(responseCode = "404",description = "not found",content = @Content)
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody RegisterDto registerDto) {
         User user = userService.createUser(registerDto);
         return ResponseEntity.ok(user);
     }
 
+    @Operation(summary = "login user")
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "sign in user", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+                    @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+                    @ApiResponse(responseCode = "403",description = "bad credentials",content = @Content),
+                    @ApiResponse(responseCode = "404",description = "not found",content = @Content)
+            }
+    )
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginDto loginDto) {
         User user = userService.loginUser(loginDto);
@@ -39,10 +60,5 @@ public class UserController {
         loginResponse.setExpiresIn(jwtService.getJwtExpiration());
 
         return ResponseEntity.ok(loginResponse);
-    }
-
-    @GetMapping("/test")
-    public String TestController() {
-        return "Test endpoint";
     }
 }
