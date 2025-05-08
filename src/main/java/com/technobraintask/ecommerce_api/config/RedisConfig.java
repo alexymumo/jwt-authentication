2d4ac9c816496850a4307fb41daa69f7f206a3a0
Alex Mumo <alexymumo2000@gmail.com>
2025-05-08 09:36:06 +0300

    Add redis feature

---
+++ src/main/java/com/technobraintask/ecommerce_api/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.technobraintask.ecommerce_api.config;
+
+import com.technobraintask.ecommerce_api.models.Product;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(Product.class))
+                );
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+
+    }
+}
--- src/main/java/com/technobraintask/ecommerce_api/service/ProductService.java
+++ src/main/java/com/technobraintask/ecommerce_api/service/ProductService.java
@@ -3,6 +3,7 @@ package com.technobraintask.ecommerce_api.service;
 import com.technobraintask.ecommerce_api.models.Product;
 import com.technobraintask.ecommerce_api.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @CachePut(value = "PRODUCT_CACHE",key = "#result.id")
     public Product createProduct(Product product) {
         return productRepository.save(product);
     }
